4654e671c6a5f73e69317d9313f10eb9d5c1c731
Mikhail Kolkov <kolkov.michael2014@yandex.ru>
2022-11-21 12:15:41 -0500

    Initial Commit

---
+++ AmexPreview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmexPreview
+//
+//  Created by Mikhail Kolkov on 11/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
